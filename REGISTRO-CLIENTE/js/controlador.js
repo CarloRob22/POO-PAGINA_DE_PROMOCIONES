fc5f142e6ff920d1ca9448b8c79ffc3c5d4a60b0
CarloRob22 <49041573+CarloRob22@users.noreply.github.com>
2020-04-01 03:12:55 -0600

    dinamismo en js de formulario de clientes

---
+++ REGISTRO-CLIENTE/js/controlador.js
@@ -0,0 +1,86 @@
+var paises = [];
+paises =[
+    {
+        pais:'Honduras',
+        departamentos:
+        [
+            'Atlántida',
+            'Choluteca',
+            'Colón',
+            'Comayagua', 
+            'Copán', 
+            'Cortes',
+            'El_Paraíso',
+            'Francisco Morazán',
+            'Gracias a Dios',
+            'Intibucá',
+            'Islas de la Bahía', 
+            'La Paz',
+            'Lempira', 
+            'Ocotepeque', 
+            'Olancho', 
+            'Santa Bárbara', 
+            'Valle', 
+            'Yoro'
+        ]
+            
+        
+    },
+    {
+        pais:'El Salvador',
+        departamentos:{
+            1:'Ahuachapán',
+            2:'Cabañas',
+            3:'Chalatenango',
+            4:'Cuscatlán',
+            5:'La Libertad',
+            6:'La Paz',
+            7:'La Unión',
+            8:'Morazán',
+            9:'San Miguel',
+            10:'San Salvador',
+            11:'San Vicente',
+            12:'Santa Ana',
+            13:'Sonsonate',
+            14:'Usulutá'
+        }
+    },
+    {
+        pais:'Guatemala',
+        departamentos:{[
+
+        ]
+            1:'Alta Verapaz',
+            2:'Baja Verapaz',
+            3:'Chimaltenango',
+            4:'Chiquimula',
+            5:'Petén',
+            6:'El Progreso',
+            7:'Quiché',
+            8:'Escuintla',            
+            9:'Guatemala',
+            10:'Huehuetenango',
+            11:'Izabal',
+            12:'Jalapa',
+            13:'Jutiapa',
+            14:'Quetzaltenango',
+            15:'Retalhuleu',
+            16:'Sacatepéquez',
+            17:'San Marcos',
+            18:'Santa Rosa',
+            19:'Sololá',
+            20:'Suchitepéquez',
+            21:'Totonicapán',
+            22:'Zacapa'
+        }
+    }
+];
+
+function pais(pais){
+    if (pais==paises[0].pais){        
+        for (let i=0;i<paises[0].departamentos.length;i++){
+
+        }
+        ``
+    }
+}
